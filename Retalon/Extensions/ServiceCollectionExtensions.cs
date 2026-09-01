6c5e35e4d1c429a8d53eb7bd64b026b7c2a408e4
Tejha <ntejha@ibm.com>
2026-09-01 11:59:58 +0530

    Phase 01 : Completed
    Folder structure created, OpenAPI configured, Scalar configured, /health works, SQL server connection configured, applicationDBcontext created, Serilog configured, File logging works, Basic DI extension structure created

---
+++ Retalon/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+namespace Retalon.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        //Future service registrations can be added here
+        return services;
+    }
+}
--- Retalon/Program.cs
+++ Retalon/Program.cs
@@ -1,7 +1,78 @@
+using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Retalon.Data;
+using Serilog;
+using Retalon.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Serilog configuration
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
+// Add services to the container.
+builder.Services.AddControllers();
+// OpenAPI support
+builder.Services.AddOpenApi();
+//Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+// Application services
+builder.Services.AddApplicationServices();
+// App building
 var app = builder.Build();
+// Serilog request logging
+app.UseSerilogRequestLogging();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// Health Check Endpoint
+app.MapGet("/health", () =>
+{
+    return Results.Ok(new
+    {
+        status = "Healthy"
+    });
+
+})
+.WithName("HealthCheck");
+
+// Health Check Endpoint for database
+
+app.MapGet("/health/database", async (ApplicationDbContext db) =>
+{
+    try
+    {
+        var canConnect = await db.Database.CanConnectAsync();
+
+        return Results.Ok(new
+        {
+            status = canConnect ? "Healthy" : "Unhealthy",
+            database = canConnect ? "Connected" : "Not Connected"
+        });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(
+            title: "Database connection failed",
+            detail: ex.Message,
+            statusCode: StatusCodes.Status503ServiceUnavailable);
+    }
+})
+.WithName("DatabaseHealthCheck");
 
 app.Run();
+
+
+
